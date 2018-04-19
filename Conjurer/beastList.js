dff99389250cc7b95bf3306f5968c3f0a7d64078
Unknown <jkarlkvist@gmail.com>
2018-04-19 10:18:49 +0200

    Initial commit

---
+++ Conjurer/beastList.js
@@ -0,0 +1,621 @@
+var beasts = [{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Blood Hawk",
+    "terrain": [
+        "Arctic",
+        "Coastal",
+        "Forest",
+        "Grassland",
+        "Hill",
+        "Mountain"
+    ]
+}, {
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Camel",
+    "terrain": [
+        "Desert"
+    ]
+}, {
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Flying Snake",
+    "terrain": [
+        "Desert",
+        "Grassland",
+        "Forest",
+        "Urban"
+    ]
+}, {
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Giant Crab",
+    "terrain": [
+        "Coastal"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Giant Rat",
+    "terrain": [
+        "Forest",
+        "Swamp",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Giant Weasel",
+    "terrain": [
+        "Forest",
+        "Grassland",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Mastiff",
+    "terrain": [
+        "Forest",
+        "Hill",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Mule",
+    "terrain": [
+        "Desert",
+        "Hill",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Poisonous Snake",
+    "terrain": [
+        "Forest",
+        "Swamp",
+        "Coastal",
+        "Desert",
+        "Hill",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Pony",
+    "terrain": [
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/8",
+    "cr_enumerable": 1,
+    "name": "Stirge",
+    "terrain": [
+        "Forest",
+        "Swamp",
+        "Coastal",
+        "Desert",
+        "Hill",
+        "Grassland",
+        "Mountain",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Axe Beak",
+    "terrain": [
+        "Hill",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Boar",
+    "terrain": [
+        "Hill",
+        "Forest",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Constrictor Snake",
+    "terrain": [
+        "Desert",
+        "Forest",
+        "Swamp",
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Draft Horse",
+    "terrain": [
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Elk",
+    "terrain": [
+        "Grassland",
+        "Forest",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Badger",
+    "terrain": [
+        "Forest"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Bat",
+    "terrain": [
+        "Forest",
+        "Underdark"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Centipede",
+    "terrain": [
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Frog",
+    "terrain": [
+        "Forest",
+        "Swamp"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Lizard",
+    "terrain": [
+        "Desert",
+        "Coastal",
+        "Forest",
+        "Swamp",
+        "Underdark"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Owl",
+    "terrain": [
+        "Arctic",
+        "Forest",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Poisonous Snake",
+    "terrain": [
+        "Desert",
+        "Forest",
+        "Grassland",
+        "Swamp",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Giant Wolf Spider",
+    "terrain": [
+        "Coastal",
+        "Desert",
+        "Forest",
+        "Grassland",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Panther",
+    "terrain": [
+        "Grassland",
+        "Forest",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Pterandon",
+    "terrain": [
+        "Coastal",
+        "Grassland",
+        "Mountain"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Riding Horse",
+    "terrain": [
+        "Grassland",
+        "Underdark"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Swarm of Bats",
+    "terrain": [
+        "Hill",
+        "Mountain",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Swarm of Rats",
+    "terrain": [
+        "Swamp",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Swarm of Ravens",
+    "terrain": [
+        "Hill",
+        "Forest",
+        "Swamp",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/4",
+    "cr_enumerable": 2,
+    "name": "Wolf",
+    "terrain": [
+        "Hill",
+        "Forest",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Ape",
+    "terrain": [
+        "Forest"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Black Bear",
+    "terrain": [
+        "Forest"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Crocodile",
+    "terrain": [
+        "Swamp",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Giant Goat",
+    "terrain": [
+        "Grassland",
+        "Hill",
+        "Mountain"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Giant Sea Horse",
+    "terrain": [
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Giant Wasp",
+    "terrain": [
+        "Forest",
+        "Grassland",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Reef Shark",
+    "terrain": [
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Ape",
+    "terrain": [
+        "Forest"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Swarm of Insects",
+    "terrain": [
+        "Desert",
+        "Forest",
+        "Grassland",
+        "Hill",
+        "Swamp",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1/2",
+    "cr_enumerable": 3,
+    "name": "Warhorse",
+    "terrain": [
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Brown Bear",
+    "terrain": [
+        "Forest",
+        "Arctic",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Dire Wolf",
+    "terrain": [
+        "Forest",
+        "Hill"
+    ]
+},
+
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Eagle",
+    "terrain": [
+        "Coastal",
+        "Grassland",
+        "Mountain",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Hyena",
+    "terrain": [
+        "Forest",
+        "Desert",
+        "Grassland",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Octopus",
+    "terrain": [
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Spider",
+    "terrain": [
+        "Forest",
+        "Desert",
+        "Swamp",
+        "Underdark",
+        "Urban"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Toad",
+    "terrain": [
+        "Forest",
+        "Coastal",
+        "Desert",
+        "Swamp",
+        "Underdark"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Giant Vulture",
+    "terrain": [
+        "Desert",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Lion",
+    "terrain": [
+        "Desert",
+        "Grassland",
+        "Mountain",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Swarm of Quippers",
+    "terrain": [
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "1",
+    "cr_enumerable": 4,
+    "name": "Tiger",
+    "terrain": [
+        "Forest",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Allosaurus",
+    "terrain": [
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Cave Bear",
+    "terrain": [
+        "Forest",
+        "Mountain",
+        "Hill"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Giant Boar",
+    "terrain": [
+        "Forest",
+        "Hill",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Giant Constrictor Snake",
+    "terrain": [
+        "Desert",
+        "Forest",
+        "Swamp",
+        "Underdark",
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Giant Elk",
+    "terrain": [
+        "Forest",
+        "Hill",
+        "Mountain",
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Hunter Shark",
+    "terrain": [
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Pleiosaurus",
+    "terrain": [
+        "Coastal",
+        "Underwater"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Polar Bear",
+    "terrain": [
+        "Arctic",
+        "Underdark"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Rhinoceros",
+    "terrain": [
+        "Grassland"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Saber-Toothed Tiger",
+    "terrain": [
+        "Arctic",
+        "Mountain"
+    ]
+},
+{
+    "combat_rating": "2",
+    "cr_enumerable": 5,
+    "name": "Swarm of Poisonous Snakes",
+    "terrain": [
+        "Forest",
+        "Swamp"
+    ]
+}
+]
---
+++ Conjurer/main.js
@@ -0,0 +1,76 @@
+window.onload = function()
+{
+
+let divData = document.getElementById("showData");
+let divRandom = document.getElementById("showRandom");
+let isClicked = false;
+
+let getCr = document.getElementById("crSelect");
+let getTerrain = document.getElementById("terrainSelect");
+let randomBtn = document.getElementById("randomBtn").addEventListener("click", getBeast);
+
+
+function getBeast(){
+    let cr = getCr.options[getCr.selectedIndex].value;
+    let tr = getTerrain.options[getTerrain.selectedIndex].text;
+    let beastArray = [];
+
+    if(isClicked){
+        divData.innerHTML = null;
+    }
+    
+    beasts.forEach(element => {
+        if(element.terrain.includes(tr) && element.cr_enumerable <= cr)
+        beastArray.push(element);
+    });
+    
+    let randomedBeast = beastArray[Math.floor(Math.random() * beastArray.length)];
+
+    if(randomedBeast !== undefined)
+    {
+        divData.innerHTML = "You summon " + getNumberOfBeasts(cr) + " " + randomedBeast.name + getPlural(cr) + " with CR " +  randomedBeast.combat_rating + "<br> Terrain: " + "<strong>" + tr + "</strong>";
+    }
+    else
+    {
+        divData.innerHTML = "Please select both a <strong>Combat Rating</strong> and a <strong>Terrain Type</strong> to request a random beast";
+    }
+
+
+    isClicked = true;
+}
+
+// determines if output string should be in plural or not depending on amount of summons
+function getPlural(crInput){
+    if(crInput < 2){
+        return "s ";
+    }
+    else{
+        return " ";
+    }
+}
+
+//gets number of beasts to summon depending on input selection
+function getNumberOfBeasts(crInput){
+    if(crInput == 1){
+        console.log(crInput);
+        return 8;
+    }
+    else if(crInput == 2){
+        console.log(crInput);
+        return 4;
+    }
+    else if(crInput == 3){
+        console.log(crInput);
+        return 2;
+    }
+    else if(crInput == 4){
+        console.log(crInput);
+        return 1;
+    }
+    else{
+        console.log(crInput);
+        return "Something went wrong";
+    }
+}
+
+};
